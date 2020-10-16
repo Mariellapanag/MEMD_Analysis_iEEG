0c93452ef9817ba78e810d40c6cefa76833dc3be
Mariella <mariella1990998@gmail.com>
2020-10-16 18:40:06 +0100

    --

--- funcs/Global_settings/global.py
+++ funcs/Global_settings/global.py
@@ -1,9 +1,7 @@
 # This is the path where the results will be stored
 # Specify the name of the folder where the results will be stored
 
-def set_folder():
-    global result_file
-    result_file = "results"
-    return result_file
+result_file = "results"
+
 
 
