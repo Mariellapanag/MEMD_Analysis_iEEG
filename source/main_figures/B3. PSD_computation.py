9fd56da0256cc759c7e66a419b75ecac216685f7
Mariella <mariella1990998@gmail.com>
2021-02-19 13:19:51 +0000

    psd_computation changes

--- source/main_figures/B3. PSD_computation.py
+++ source/main_figures/B3. PSD_computation.py
@@ -63,7 +63,6 @@ def process_file(in_path):
     fs_limit = 2880 / 2
     binsfreq = np.logspace ( -3,  np.log10 ( fs_limit ), 800, endpoint=True )
     imf_bin_mean = np.zeros ( shape=(n_imfs, binstime[:-1].shape[0], binsfreq.shape[0]-1) )
-    all_imf_comp = np.zeros(shape=(n_imfs, n_comp ,binstime[:-1].shape[0], binsfreq.shape[0]-1))
     for imf in range ( 0, n_imfs ):
         allComp_mean = np.zeros ( shape=(n_comp, binstime[:-1].shape[0], binsfreq.shape[0]-1) )
         for comp in range ( 0, n_comp ):
@@ -74,39 +73,16 @@ def process_file(in_path):
                                                                                bins=[binstime, binsfreq],
                                                                                expand_binnumbers=True )
             allComp_mean[comp, :, :] = statistic
-            all_imf_comp[imf, comp, :, :] = statistic
-        imf_bin_mean[imf, :, :] = np.nanmean( allComp_mean, axis=0 )/2
+        imf_bin_mean[imf, :, :] = np.nanmean( allComp_mean, axis=0 )
 
+    id_nan = np.where(np.isnan(imf_bin_mean))
+    imf_bin_mean[id_nan] = 0
     '''Marginal Density of each Comp within IMF'''
 
-    all_imfs_comps = (np.nansum(all_imf_comp, axis = 2))/(2*time[:-1].shape[0])
-    # indx = np.where(all_imfs<10)
-    # all_imfs_comps[indx] = 0
+    id_nan = np.where(np.isnan(imf_bin_mean))
+    imf_bin_mean[id_nan] = 0
 
     bin_cntrs_y = (y_edge[1:] + y_edge[:-1])/2
-
-    # Plot imfs
-    for imf in range(0, n_imfs):
-        ############################################
-        # Plot all IMFs
-        fig_name = "Marginal_Hilbert_IMF{}_{}.{}".format (imf+1, id_patient, "pdf" )
-        with PdfPages(os.path.join(out_subfolder, fig_name)) as pages:
-            for comp in range(0, n_comp):
-                fig, ax = plt.subplots (figsize=(10, 8)  )
-                plt.plot (bin_cntrs_y, all_imfs_comps[imf,comp, :], alpha=0.7, label="IMF{}".format(imf+1) , rasterized = True)
-                plt.xlabel ( "Frequency (cycles/day)" )
-                plt.title('Comp{}'.format(comp+1))
-                plt.ylabel ( "Power" )
-                plt.xscale ( "log" )
-                plt.yscale ( "log" )
-                # plt.xlim(0.1, 100)
-                # plt.ylim(0.0001, 1)
-                plt.ylim ( 1e-09, 1 )
-                plt.legend ()
-                plt.tight_layout ()
-                canvas = FigureCanvasPdf(fig)
-                canvas.print_figure(pages)
-                plt.close("all")
 
     """
     Hilbert Huang Transform as a 2D representation - PSD
@@ -177,40 +153,44 @@ def process_file(in_path):
     indx = np.where(allImf_count<40)
     all_imfs[indx] = 0
 
-    bin_cntrs_y = (y_edge[1:] + y_edge[:-1])/2
-    # ######Plot imfs
+    '''Plot imfs'''
+    # colors = ["#0a3955","#174c6f", "#1b5b85", "#3b526d", "#466484", "#4e7399",
+    #           "#68738c", "#7484a0", "#7c92b0", "#969cae", "#a1aabd", "#a8b3c9", "#c9cbd5", "#cfd2de",
+    #           "#d4d8e4","#bfbdbb"]
     colors = []
-    fig, ax = plt.subplots ( figsize=(10, 8) )
-
-    for imf in range(1, n_imfs-1):
-
-        plot = ax.plot(bin_cntrs_y, all_imfs[imf,:], alpha=0.7, label="IMF{}".format(imf+1) )
+    fig, ax = plt.subplots ( figsize=(6,4) )
+    ax.set_facecolor('xkcd:white')
+    for imf in range(0, n_imfs):
+        # plot = plt.plot(bin_cntrs_y, all_imfs[imf,:], alpha=0.7, label="IMF{}".format(imf+1),
+        #                color = colors[imf] , linewidth = 1)
+        plot = plt.plot(bin_cntrs_y, all_imfs[imf,:], alpha=0.7, label="IMF{}".format(imf+1), linewidth = 1)
         colors.append(plot[0].get_color ())
-        ax.legend ( loc='center left', bbox_to_anchor=(1.1, 0.5) )
-    total = np.nansum(all_imfs[1:-1, :], axis=0)
-    plt.plot(bin_cntrs_y, total,  label="SUM", alpha=0.8, color="black" )
+    total = np.sum(all_imfs, axis=0)
+    # total = np.sum(all_imfs[:-1,], axis=0)
+    plt.plot(np.squeeze(bin_cntrs_y), np.squeeze(total),  label="SUM", alpha=0.8, color="black" )
+    ax.tick_params(axis = 'both', which = 'major', labelsize = 8)
+    ax.tick_params(axis = 'both', which = 'minor', labelsize = 8)
     plt.xlabel ( "Frequency (cycles/day)" )
     plt.ylabel ( "Power" )
     plt.xscale ( "log" )
     plt.yscale ( "log" )
-    # plt.xlim(0.1, 100)
-    # plt.ylim(0.0001, 1)
-    plt.ylim ( 1e-09, 1 )
-    plt.legend ()
+    ax.legend (loc='center left', bbox_to_anchor=(1, 0.5))
+    #plt.xlim(0.1, 100)
+    plt.xlim(0.001, bin_cntrs_y.max())
+    plt.ylim ( 1e-07, 1e-03 )
     plt.tight_layout ()
-
     format = "pdf"
-    name = "AllIMFs_psd{}".format (id_patient )
+    name = "AllIMFs_psd_last{}".format (subfolder )
     fig_name = "{}.{}".format ( name, format )
-    plt.savefig ( os.path.join ( out_subfolder, fig_name ) )
+    #fig.set_size_inches(66/25.4, 54/25.4)
+    plt.savefig ( os.path.join ( path_results, fig_name ), dpi = 100)
     plt.close ( 'all' )
 
-    ############################################
-    # Plot all IMFs
-    fig_name = "EachIMF_{}.{}".format (id_patient, "pdf" )
-    with PdfPages(os.path.join(out_subfolder, fig_name)) as pages:
+    '''Plot all IMFs'''
+    fig_name = "EachIMF_{}.{}".format (subfolder, "pdf" )
+    with PdfPages(os.path.join(path_results, fig_name)) as pages:
         i=0
-        for imf in range(1, n_imfs-1):
+        for imf in range(0, n_imfs):
             fig, ax = plt.subplots (figsize=(10, 8)  )
             plt.plot (bin_cntrs_y, all_imfs[imf,:], alpha=0.7, color = colors[i], label="IMF{}".format(imf+1) , rasterized = True)
             plt.xlabel ( "Frequency (cycles/day)" )
@@ -218,8 +198,8 @@ def process_file(in_path):
             plt.xscale ( "log" )
             plt.yscale ( "log" )
             # plt.xlim(0.1, 100)
-            # plt.ylim(0.0001, 1)
-            plt.ylim ( 1e-09, 1 )
+            plt.ylim(0.001, bin_cntrs_y.max())
+            plt.ylim ( 1e-07, 1e-03 )
             plt.legend ()
             plt.tight_layout ()
             canvas = FigureCanvasPdf(fig)
@@ -227,26 +207,57 @@ def process_file(in_path):
             plt.close("all")
             i = i+1
 
+    '''Plot imfs without last IMF'''
+    # colors = ["#0a3955","#174c6f", "#1b5b85", "#3b526d", "#466484", "#4e7399",
+    #           "#68738c", "#7484a0", "#7c92b0", "#969cae", "#a1aabd", "#a8b3c9", "#c9cbd5", "#cfd2de",
+    #           "#d4d8e4","#bfbdbb"]
+    colors = []
+    fig, ax = plt.subplots ( figsize=(6,4) )
+    ax.set_facecolor('xkcd:white')
+    for imf in range(0, n_imfs-1):
+        # plot = plt.plot(bin_cntrs_y, all_imfs[imf,:], alpha=0.7, label="IMF{}".format(imf+1),
+        #                color = colors[imf] , linewidth = 1)
+        plot = plt.plot(bin_cntrs_y, all_imfs[imf,:], alpha=0.7, label="IMF{}".format(imf+1), linewidth = 1)
+        colors.append(plot[0].get_color ())
+    total = np.sum(all_imfs[:-1,:], axis=0)
+    # total = np.sum(all_imfs[:-1,], axis=0)
+    plt.plot(np.squeeze(bin_cntrs_y), np.squeeze(total),  label="SUM", alpha=0.8, color="black" )
+    ax.tick_params(axis = 'both', which = 'major', labelsize = 8)
+    ax.tick_params(axis = 'both', which = 'minor', labelsize = 8)
+    plt.xlabel ( "Frequency (cycles/day)" )
+    plt.ylabel ( "Power" )
+    plt.xscale ( "log" )
+    plt.yscale ( "log" )
+    ax.legend (loc='center left', bbox_to_anchor=(1, 0.5))
+    #plt.xlim(0.1, 100)
+    plt.xlim(0.001, bin_cntrs_y.max())
+    plt.ylim ( 1e-07, 1e-03 )
+    plt.tight_layout ()
+    format = "pdf"
+    name = "AllIMFs_psd_wo_lastIMF{}".format (subfolder )
+    fig_name = "{}.{}".format ( name, format )
+    #fig.set_size_inches(66/25.4, 54/25.4)
+    plt.savefig ( os.path.join ( path_results, fig_name ), dpi = 100)
+    plt.close ( 'all' )
 
-    ################################################################
-    # Compute and save the dominant frequency based on the above calculation
-    idfreq = np.nanargmax ( all_imfs, axis=1 )
-    dominant_frequency = np.zeros(n_imfs)
+    '''Compute and save the dominant frequency based on the above calculation'''
+    idfreq = np.argmax ( all_imfs, axis=1 )
+    dominant_frequency = np.empty(n_imfs)
+    lhh = np.empty(n_imfs)
     for imf in range(0, n_imfs):
         id = idfreq[imf]
         dominant_frequency[imf] = bin_cntrs_y[id]
-
-    # Save the dominant frequency for all IMFs
-    d_freq = {'dom_freq': dominant_frequency}
-    sio.savemat ( os.path.join ( out_subfolder, "dominant_Psdfreq_allIMFs_{}.mat".format ( id_patient ) ), d_freq )
-
-    #################################################################
-    # Compute and save the total SUM for each patient
-    total_psd = {'total_SUM': np.nansum(all_imfs[1:-1, :], axis=0), "freqseq": binsfreq, "freq_edge": y_edge, "bin_cntrs_freq": bin_cntrs_y}
-    sio.savemat ( os.path.join ( out_subfolder, "Total_SUM_IMFs_wo_1andLast{}.mat".format ( id_patient ) ), total_psd )
-
-    total_psd1 = {'total_SUM': np.nansum(all_imfs, axis=0), "freqseq": binsfreq, "freq_edge": y_edge, "bin_cntrs_freq": bin_cntrs_y}
-    sio.savemat ( os.path.join ( out_subfolder, "Total_SUM_IMFs_{}.mat".format ( id_patient ) ), total_psd1 )
+        lhh[imf] = all_imfs[imf, id]
+    '''Save the dominant frequency for all IMFs'''
+    d_freq = {'dom_freq': dominant_frequency, "MarginalHH": lhh}
+    sio.savemat ( os.path.join ( out_subfolder, "dominant_Psdfreq_allIMFs_{}.mat".format ( subfolder ) ), d_freq )
+    '''Compute and save the total SUM for each patient'''
+    total_psd = {'total_SUM': np.sum(all_imfs[1:-1, :], axis=0), "freqseq": binsfreq, "freq_edge": y_edge, "bin_cntrs_freq": bin_cntrs_y}
+    sio.savemat ( os.path.join ( out_subfolder, "Total_SUM_IMFs_wo_1andLast{}.mat".format ( subfolder ) ), total_psd )
+    total_psd1 = {'total_SUM': np.sum(all_imfs, axis=0), "freqseq": binsfreq, "freq_edge": y_edge, "bin_cntrs_freq": bin_cntrs_y}
+    sio.savemat ( os.path.join ( out_subfolder, "Total_SUM_IMFs_{}.mat".format ( subfolder ) ), total_psd1 )
+    total_psd2 = {'total_SUM': np.sum(all_imfs[0:-1,:], axis=0), "freqseq": binsfreq, "freq_edge": y_edge, "bin_cntrs_freq": bin_cntrs_y}
+    sio.savemat ( os.path.join ( out_subfolder, "Total_SUM_IMFs_wo_LastIMF{}.mat".format ( subfolder ) ), total_psd2 )
 
 
 def parallel_process():
